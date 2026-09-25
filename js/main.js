cc7bca21e7472f4005518cac1a138bad1f8889da
Om Dhakane <omdhakane21@gmail.com>
2026-09-25 18:42:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API="http://127.0.0.1:8000";
+const API="https://essa-hwxl.onrender.com";
 document.getElementById("year").textContent=new Date().getFullYear();
 const menu=document.getElementById("mobileMenu");
 if(menu) menu.onclick=()=>document.querySelector(".navbar").classList.toggle("open");
